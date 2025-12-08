f598d740df3de3fb31e0406fe360128a497e6f07
Reve <reveabikhalil@gmail.com>
2025-12-08 12:29:44 +0200

    Add to cart functionality

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,7 @@
 class StoreController < ApplicationController
   def index
+    session[:counter].nil? ? session[:counter] = 1 : session[:counter] += 1
+    @counter = session[:counter]
     @products = Product.order(:title)
   end
 end
